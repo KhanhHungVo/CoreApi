01536bbb9d0f7e3ffc4f19b87a0c0fb3e2376018
Hung Vo <Hung.Khanh.Vo@steadfasttech.com.au>
2018-12-04 23:24:58 +0700

    add user dto & apply db context base

--- DTOs/UserDTO.cs
+++ DTOs/UserDTO.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CoreApi.Entities
+namespace CoreApi.DTOs
 {
-    public class User
+    public class UserDTO
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string Token { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
     }
 }
